aa882d735761fac6d604d9660e51fd956cd9faa6
umakant1999 <umakantranga47@gmail.com>
2026-08-12 18:06:56 +0530

    javascript commit

---
+++ javascript/day1.js
@@ -0,0 +1,23 @@
+
+console.log("hello my name is umakant ")
+console.log(5+10);
+console.error("this is error ")
+console.warn("this is warning")
+
+
+//t
+console.log("hello javascript")
+
+//changed
+
+console.log("hello,worldd")
+
+
+
+
+
+
+
+
+
+
---
+++ javascript/day2.js
@@ -0,0 +1,121 @@
+//data types
+
+// number
+
+// let age =27;
+// let score =0;
+
+//string
+// let name ="umakant"
+
+//boolean
+// let isLoggin  =true
+// let hasPhone  =false
+
+//array
+let arr =[1,2,3,4,5,6]
+console.log(arr.splice(0,2))
+// console.log(arr.join("!")) its convert array into a string
+
+
+console.log(typeof arr.join())
+
+// 3. Operators
+// +   // addition
+// -   // subtraction
+// *   // multiplication
+// /   // division
+// %   // remainder
+// **  // power
+
+// Comparison Operators
+
+// Result hamesha true/false hota hai.
+
+// >    // greater
+// <    // smaller
+// >=   // greater/equal
+// <=   // smaller/equal
+// ==   // equal
+// ===  // strictly equal
+// !=   // not equal
+// !==  // strictly not equal
+
+
+// Logical Operators — "Multiple conditions"
+
+// 1.&& and
+
+// true && true = true
+// true && false = false
+
+
+// 2.|| or
+// true && true = true
+// true && false = true
+
+// 3.NOT !
+// !true  = false;
+// !false = true
+
+
+// conditons
+
+// let age = 20;
+
+// if (age >= 18) {
+//     console.log("Adult");
+// }
+
+
+// multiple conditions
+
+// let marks = 75;
+
+// if (marks >= 90) {
+//     console.log("A+");
+// } else if (marks >= 75) {
+//     console.log("A");
+// } else if (marks >= 50) {
+//     console.log("B");
+// } else {
+//     console.log("Fail");
+// }
+
+// templete literals
+
+// let name = "Umakant";
+// let age = 27;
+
+// console.log(`My name is ${name} and my age is ${age}`);
+//ternary operators 
+
+// result = age >= 18 ? "Adult" : "Minor";
+
+//accesing a single element from an array
+// arr[1]
+
+//add elements in array from last
+// arr.push()
+
+//addimg element from start
+// arr.unshift()
+
+
+// print all element 
+
+// for(let i = 0; i<arr.length; i++){
+//     console.log(arr[i])
+// }
+
+//delete element from last
+// arr.pop()
+// //delete element from start
+
+// arr.shift();
+
+// console.log(arr.slice(1,4))
+// console.log(arr.splice(0,2))
+//object
+
+
